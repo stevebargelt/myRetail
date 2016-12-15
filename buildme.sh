2479de07c594c4b64c10ea840a548e1992f3a6fc
Steve Bargelt <steve@bargelt.com>
2016-12-14 16:48:45 -0800

    Cleaning up unnecessary code

--- buildme.sh
+++ buildme.sh
@@ -1,22 +1,12 @@
 #!bin/bash
 
-# docker run --name mongo -p 27017:27017 -v $PWD/data:/data/db -d mongo
-
-
-# dotnet restore
-# dotnet test test/myRetail.Tests/project.json
-# dotnet publish src/myRetail/project.json -c release -o $(pwd)/publish/
-# docker build -t myretail publish
-# docker stop myRetail || true && docker rm myRetail || true
-# docker run -d --name myRetail --link mongo:mongo -p 8001:80 myretail
-
-
-#docker build -t mongo-seed mongo-seed
-#docker run --rm --name mongo-seed --link mongo:mongo mongo-seed
-
 dotnet restore
 dotnet publish src/myRetail/project.json -c release -o $(pwd)/publish/
 docker build -t myretail publish
+docker stop mongo || true && docker rm mongo || true
+docker run --name mongo -p 27017:27017 -v $PWD/data:/data/db -d mongo
 docker stop myRetail || true && docker rm myRetail || true
 docker run -d --name myRetail --link mongo:mongo -p 8001:80 myretail
 
+docker build -t mongo-seed mongo-seed
+docker run --rm --name mongo-seed --link mongo:mongo mongo-seed
